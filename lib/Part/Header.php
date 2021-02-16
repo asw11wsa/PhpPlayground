76e1289cfa80123466776128fd15beb3f2dfe64c
asw11wsa <asw11wsa@naver.com>
2021-02-16 13:47:55 +0900

    change html and css

--- lib/Part/Header.php
+++ lib/Part/Header.php
@@ -6,7 +6,7 @@ class Header {
     function __construct()
     {
         echo '<header>
-                  <div>this is header</div>
+                  <div class="logo">this is header</div>
                   <a href="/index.php">home</a>
                   <a href="/index.php?subject=topic">topic</a>
                   <a href="/index.php?subject=community">community</a>
